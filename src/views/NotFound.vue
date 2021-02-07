072ae39d55cbd667dc5b3869ff714b47b4d51aed
Gouson <gouson@qq.com>
2021-02-07 17:59:49 +0800

    .

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    当前页面不存在请检查路径
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
