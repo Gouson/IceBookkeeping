3e5d211dd580d781ab60a2169be199a963f8b01b
Gouson <gouson@qq.com>
2021-02-08 17:54:46 +0800

    eslint config;label import

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link> |
-    <router-link to="/labels">标签</router-link>|
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#money" />
+      </svg>
+      记账</router-link
+    >
+    |
+    <router-link to="/labels"
+      ><svg>
+        <use xlink:href="#label" /></svg
+      >标签</router-link
+    >
+    |
     <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
 <script>
-export default {};
+// import label from "@/assets/icons/label.svg";
+// import money from "@/assets/icons/money.svg";
+// import statistics from "@/assets/icons/statistics.svg";
+const importAll = (requireContext) =>
+  requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context("../assets/icons", true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+
+export default {
+  name: "Nav",
+};
 </script>
 
 <style lang="scss" scoped>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const path = require('path')
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons') //配置所有icon的地址
+    //config就是vue把webpack的api给封装了
+    config.module
+      .rule('svg-sprite')
+      .test(/\.svg$/)
+      .include.add(dir).end() //包含icons目录
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({
+        extract: false
+      }).end()
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{
+      plainSprite: true
+    }])
+    config.module.rule('svg').exclude.add(dir) //其他svg loader排除icons目录
+  }
 }
