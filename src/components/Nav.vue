b95fc6c92fe8940c1269b7786179219a96318ec5
Gouson <gouson@qq.com>
2021-02-18 11:18:28 +0800

    Icon样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="nav">
-    <router-link to="/money"><Icon name="money" />记账</router-link>
-    |
-    <router-link to="/labels"><Icon name="labels" />标签</router-link>
-    |
-    <router-link to="/statistics"><Icon name="statistics" />统计</router-link>
-  </div>
+  <nav>
+    <router-link class="item" to="/money">
+      <Icon class="icon" name="money" />记账
+    </router-link>
+    <router-link class="item" to="/labels">
+      <Icon class="icon" name="labels" />标签
+    </router-link>
+    <router-link class="item" to="/statistics">
+      <Icon class="icon" name="statistics" />统计
+    </router-link>
+  </nav>
 </template>
 
 <script>
@@ -15,7 +19,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav {
+nav {
   border: 1px solid red;
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  font-size: 12px;
+  > .item {
+    width: 33.333333%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2px 0;
+    .icon {
+      height: 32px;
+      width: 32px;
+    }
+  }
 }
 </style>
