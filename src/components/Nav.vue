f5c5884b5a9790b1785b495b0c7be1b3bed231b7
Gouson <gouson@qq.com>
2021-02-18 17:34:46 +0800

    money页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -19,9 +19,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 nav {
   display: flex;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  @extend %outerShadow;
   font-size: 12px;
   > .item {
     width: 33.333333%;
