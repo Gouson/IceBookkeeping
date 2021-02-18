b6a65fe48787e8853a74764e91d51368b7f79f95
Gouson <gouson@qq.com>
2021-02-18 10:47:14 +0800

    创建Icon组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="'#' + name" />
+  </svg>
+</template>
+
+<script>
+// import label from "@/assets/icons/label.svg";
+// import money from "@/assets/icons/money.svg";
+// import statistics from "@/assets/icons/statistics.svg";
+import Icon from "@/components/Icon.vue";
+// 引入整个文件夹
+const importAll = (requireContext) =>
+  requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context("../assets/icons", true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+export default {
+  props: ["name"],
+  name: "Icon",
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,34 +1,14 @@
 <template>
   <div class="nav">
-    <router-link to="/money">
-      <svg>
-        <use xlink:href="#money" />
-      </svg>
-      记账</router-link
-    >
+    <router-link to="/money"><Icon name="money" />记账</router-link>
     |
-    <router-link to="/labels"
-      ><svg>
-        <use xlink:href="#label" /></svg
-      >标签</router-link
-    >
+    <router-link to="/labels"><Icon name="labels" />标签</router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics"><Icon name="statistics" />统计</router-link>
   </div>
 </template>
 
 <script>
-// import label from "@/assets/icons/label.svg";
-// import money from "@/assets/icons/money.svg";
-// import statistics from "@/assets/icons/statistics.svg";
-const importAll = (requireContext) =>
-  requireContext.keys().forEach(requireContext);
-try {
-  importAll(require.context("../assets/icons", true, /\.svg$/));
-} catch (error) {
-  console.log(error);
-}
-
 export default {
   name: "Nav",
 };
