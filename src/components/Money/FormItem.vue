2e74d4408446e1c6fe78b071daa451fea6a3ead4
Gouson <gouson@qq.com>
2021-02-23 15:11:33 +0800

    bug

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -2,20 +2,24 @@
   <div>
     <label class="notes">
       <span class="name">{{ this.fieldName }}</span>
-      <input type="text" :value="value" @input="onValueChanged($event.target.value)" :placeholder="this.placeholder" />
+      <input
+        type="text"
+        :value="value"
+        @input="onValueChanged($event.target.value)"
+        :placeholder="placeholder"
+      />
     </label>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { Component, Prop, Watch } from 'vue-property-decorator';
+import { Component, Prop } from 'vue-property-decorator';
 @Component
 export default class FormItem extends Vue {
   @Prop({ default: '' }) value!: string;
   @Prop({ required: true }) fieldName!: string;
   @Prop() placeholder?: string;
-  @Watch('value')
   onValueChanged(value: string) {
     this.$emit('update:value', value);
   }
